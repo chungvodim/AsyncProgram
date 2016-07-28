657f2f0c8d202ff362f8cb101703031300f6491e
chungvodim <ba198647@gmail.com>
2016-07-28 16:22:06 +0700

    u

--- TaskRelationships/Program.cs
+++ TaskRelationships/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TaskRelationships
@@ -12,10 +13,47 @@ namespace TaskRelationships
         {
             //Chaining Tasks (Continuations)
             int i = 42;
-            Task<int> firstTask = Task.Factory.StartNew<int>((ci) => { Console.WriteLine("First Task take {0}", i); return (int)(ci) + 1; }, i);
+            Task<int> firstTask = Task.Factory.StartNew<int>((ci) => { Console.WriteLine("First Task take {0}", i); /*throw new Exception("Errorrrrrrrr") ;*/ return (int)(ci) + 1; }, i);
             Task secondTask = firstTask.ContinueWith(ft => { Console.WriteLine("Second Task, First task returned {0}", ft.Result); }, TaskContinuationOptions.OnlyOnRanToCompletion);
-            Task errorHandler = firstTask.ContinueWith(ft => Console.WriteLine(ft.Exception), TaskContinuationOptions.OnlyOnFaulted);
-            secondTask.Wait();
+            Task errorHandler = firstTask.ContinueWith(ft => Console.WriteLine(ft.Exception.InnerException.Message), TaskContinuationOptions.OnlyOnFaulted);
+
+            Console.WriteLine("Do independent things");
+            Console.WriteLine("start waiting");
+
+            try
+            {
+                secondTask.Wait();
+                errorHandler.Wait();
+            }
+            catch (AggregateException errors)
+            {
+                foreach (Exception error in errors.Flatten().InnerExceptions)
+                {
+                    Console.WriteLine("fuck {0} : {1}", error.GetType().Name, error.Message);
+                }
+            }
+
+            //Nested Task
+            Task.Factory.StartNew(() =>
+            {
+                Task childTask = Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("start child task1");
+                    Thread.Sleep(10000);
+                    Console.WriteLine("finish child task1");
+                });
+            }).Wait();
+
+            Task.Factory.StartNew(() => 
+            {
+                Task childTask = Task.Factory.StartNew(() => 
+                {
+                    Console.WriteLine("start child task2");
+                    Thread.Sleep(5000);
+                    Console.WriteLine("finish child task2");
+                }, TaskCreationOptions.AttachedToParent);
+            }).Wait();
+            Console.WriteLine("finish waiting");
         }
     }
 }
---
+++ ThreadSafety/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadSafety
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int iterations = 100000;
+            const int numTasks = 2;
+            List<Task> tasks = new List<Task>();
+            int value = 0;
+            for (int nTask = 0; nTask < numTasks; nTask++)
+            {
+                Task t = Task.Factory.StartNew(() =>
+                {
+                    IncrementValue(ref value, iterations);
+                });
+                tasks.Add(t);
+            }
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine("Expected value: {0}, Actual value: {1}", numTasks * iterations, value);
+        }
+        private static void IncrementValue(ref int value, int iterations)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                value++;
+            }
+        }
+
+    }
+}
