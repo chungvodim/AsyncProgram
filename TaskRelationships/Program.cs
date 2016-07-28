10f35882455835bd99dc2c3b7483066ee214ede5
chungvodim <ba198647@gmail.com>
2016-07-28 10:32:17 +0700

    u

--- TaskRelationships/Program.cs
+++ TaskRelationships/Program.cs
@@ -10,9 +10,10 @@ namespace TaskRelationships
     {
         static void Main(string[] args)
         {
+            //Chaining Tasks (Continuations)
             int i = 42;
             Task<int> firstTask = Task.Factory.StartNew<int>((ci) => { Console.WriteLine("First Task take {0}", i); return (int)(ci) + 1; }, i);
-            Task secondTask = firstTask.ContinueWith(ft => { Console.WriteLine("Second Task, First task returned {0}", ft.Result); });
+            Task secondTask = firstTask.ContinueWith(ft => { Console.WriteLine("Second Task, First task returned {0}", ft.Result); }, TaskContinuationOptions.OnlyOnRanToCompletion);
             Task errorHandler = firstTask.ContinueWith(ft => Console.WriteLine(ft.Exception), TaskContinuationOptions.OnlyOnFaulted);
             secondTask.Wait();
         }
