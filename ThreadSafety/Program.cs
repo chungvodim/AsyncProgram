0a4f6a1452370ab4fa18cf08b04239818d574f41
chungvodim <ba198647@gmail.com>
2016-07-28 18:06:26 +0700

    ThreadSafety.csproj

--- ThreadSafety/Program.cs
+++ ThreadSafety/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ThreadSafety
@@ -10,7 +11,25 @@ namespace ThreadSafety
     {
         static void Main(string[] args)
         {
-            const int iterations = 100000;
+            List<Task> tasks = new List<Task>();
+            SmallBusiness sb = new SmallBusiness(1000, 1000);
+            for (int nTask = 0; nTask < 10; nTask++)
+            {
+                Task t = Task.Factory.StartNew(() =>
+                {
+                    sb.UnsafeReceivePayment(10);
+                    Console.WriteLine("NetWorth: {0}", sb.UnsafeNetWorth);
+                });
+                //t.Wait();
+                tasks.Add(t);
+            }
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine("finish waiting");
+        }
+
+        private static void TestInterLocked()
+        {
+            const int iterations = 100000000;
             const int numTasks = 2;
             List<Task> tasks = new List<Task>();
             int value = 0;
@@ -18,20 +37,36 @@ namespace ThreadSafety
             {
                 Task t = Task.Factory.StartNew(() =>
                 {
-                    IncrementValue(ref value, iterations);
+                    IncrementValue(ref value, iterations, true);
                 });
                 tasks.Add(t);
             }
             Task.WaitAll(tasks.ToArray());
             Console.WriteLine("Expected value: {0}, Actual value: {1}", numTasks * iterations, value);
         }
-        private static void IncrementValue(ref int value, int iterations)
+
+        // The Interlocked static class provides methods to turn nonatomic operations into atomic ones. It generally only works on single pieces of data up to 64 bits in length
+        // Interlocked has three basic operations: Increment, Decrement, and Add 
+        // Interlocked will affect code run significantly slower (4 to 10 times slower depending on hardware)
+        private static void IncrementValue(ref int value, int iterations, bool hasInterlocked)
         {
-            for (int i = 0; i < iterations; i++)
+            if (hasInterlocked)
             {
-                value++;
+                for (int i = 0; i < iterations; i++)
+                {
+                    Interlocked.Increment(ref value);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < iterations; i++)
+                {
+                    //1.Copy the value from the variable into a register
+                    //2.Increment the value in the register
+                    //3.Copy the new value to the variable
+                    value++;
+                }
             }
         }
-
     }
 }
---
+++ ThreadSafety/SmallBusiness.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSafety
+{
+    public class SmallBusiness
+    {
+        private decimal cash;
+        private decimal receivables;
+        //Objects are cheap to allocate, and so the simplest way to ensure you only have contention where necessary is to use private instance or static variables, depending on whether the state to be protected is instance or static data,respectively
+        private readonly object stateGuard = new object();
+        public SmallBusiness(decimal cash, decimal receivables)
+        {
+            this.cash = cash;
+            this.receivables = receivables;
+        }
+
+        public void UnsafeReceivePayment(decimal amount)
+        {
+            cash += amount;
+            receivables -= amount;
+        }
+
+        public void SafeReceivePayment(decimal amount)
+        {
+            // dead lock may happen
+            lock (stateGuard)
+            {
+                cash += amount;
+                receivables -= amount;
+            }
+        }
+
+        public void SaferReceivePayment(decimal amount)
+        {
+            //need to ensure that you always release the monitor, and for that you can use a try . . . finally block => prevent dead lock (locked state forever)
+            bool lockTaken = false;
+            try
+            {
+                Monitor.Enter(stateGuard, ref lockTaken);
+                cash += amount;
+                receivables -= amount;
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    Monitor.Exit(stateGuard);
+                }
+            }
+        }
+
+        public void ReceivePayment(decimal amount)
+        {
+            bool lockTaken = false;
+            try
+            {
+                // Timeout out of Waits Using Monitor.TryEnter
+                Monitor.TryEnter(stateGuard, TimeSpan.FromSeconds(30), ref lockTaken);
+                if (lockTaken)
+                {
+                    cash += amount;
+                    receivables -= amount;
+                }
+                else
+                {
+                    throw new TimeoutException("Failed to acquire stateGuard");
+                }
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    Monitor.Exit(stateGuard);
+                }
+            }
+        }
+
+
+        public decimal SafeNetWorth
+        {
+            get
+            {
+                Monitor.Enter(stateGuard);
+                decimal netWorth = cash + receivables;
+                Monitor.Exit(stateGuard);
+                return netWorth;
+            }
+        }
+
+        public decimal UnsafeNetWorth
+        {
+            get
+            {
+                decimal netWorth = cash + receivables;
+                return netWorth;
+            }
+        }
+    }
+}
