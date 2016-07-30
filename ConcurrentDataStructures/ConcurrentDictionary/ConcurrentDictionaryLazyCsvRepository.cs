fddc0dbab1267046883680b82f9fed4a2aa5af19
chungvodim <ba198647@gmail.com>
2016-07-30 15:05:12 +0700

    u

--- ConcurrentDataStructures/ConcurrentDictionary/ConcurrentDictionaryLazyCsvRepository.cs
+++ ConcurrentDataStructures/ConcurrentDictionary/ConcurrentDictionaryLazyCsvRepository.cs
@@ -11,33 +11,42 @@ namespace ConcurrentDataStructures.ConcurrentDictionary
     public class ConcurrentDictionaryLazyCsvRepository
     {
         private readonly string directory;
-        private ConcurrentDictionary<string, List<string[]>> csvFiles;
+        private ConcurrentDictionary<string, Lazy<List<string[]>>> csvFiles;
         public ConcurrentDictionaryLazyCsvRepository(string directory)
         {
             this.directory = directory;
-            csvFiles = new ConcurrentDictionary<string, List<string[]>>();
+            csvFiles = new ConcurrentDictionary<string, Lazy<List<string[]>>>();
         }
         public IEnumerable<string> Files
         {
             get { return new DirectoryInfo(directory).GetFiles().Select(fi => fi.FullName); }
         }
+
         public IEnumerable<T> Map<T>(string dataFile, Func<string[], T> map)
         {
-            List<string[]> csvFile;
-            if (!csvFiles.TryGetValue(dataFile, out csvFile))
-            {
-                csvFile = LoadData(dataFile).ToList();
-                // TryAdd method, which will return false if the add fails due to the key already being present
-                csvFiles.TryAdd(dataFile, csvFile);
-            }
-            return csvFile.Skip(1).Select(map);
+            var csvFile = new Lazy<List<string[]>>(() => LoadData(dataFile).ToList());
+            // GetOrAdd = TryGetValue + TryAdd
+            csvFile = csvFiles.GetOrAdd(dataFile, csvFile);
+            return csvFile.Value.Skip(1).Select(map);
         }
+        //public IEnumerable<T> Map<T>(string dataFile, Func<string[], T> map)
+        //{
+        //    List<string[]> csvFile;
+        //    if (!csvFiles.TryGetValue(dataFile, out csvFile))
+        //    {
+        //        csvFile = LoadData(dataFile).ToList();
+        //        // TryAdd method, which will return false if the add fails due to the key already being present
+        //        csvFiles.TryAdd(dataFile, csvFile);
+        //    }
+        //    return csvFile.Skip(1).Select(map);
+        //}
 
-        public IEnumerable<T> Map1<T>(string dataFile, Func<string[], T> map)
-        {
-            List<string[]> csvFile = csvFiles.GetOrAdd(dataFile, df => LoadData(df).ToList());
-            return csvFile.Skip(1).Select(map);
-        }
+        //public IEnumerable<T> Map1<T>(string dataFile, Func<string[], T> map)
+        //{
+        //    // GetOrAdd = TryGetValue + TryAdd
+        //    List<string[]> csvFile = csvFiles.GetOrAdd(dataFile, df => LoadData(df).ToList());
+        //    return csvFile.Skip(1).Select(map);
+        //}
 
         private IEnumerable<string[]> LoadData(string filename)
         {
